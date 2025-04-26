01b23ffb97df0fe8a5cc85e0f50ee478ffbf1089
Mnac99 <mnac.tigranyan@gmail.com>
2025-04-27 00:08:25 +0400

    after adding to cart functionality

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,34 @@
-
+import "./ProductStyle.css"
+import {CartContext} from "./CreateContext.jsx";
+import {useContext} from "react";
+import {useState} from "react";
+import products from "./Products.jsx";
 const Product = ({price,id,title,image,category}) => {
+    const {cart,setCart} = useContext(CartContext);
+
+    const addToCart =() => {
+        const productItem = {
+            id,
+            title,
+            price,
+            image,
+            category
+        };
+        const checker = cart.some((item) => item.id === productItem.id);
+        if(!checker) {
+            setCart([...cart, productItem]);
+        }
+    }
+
     return (
         <div key={id} className="product">
+            <img src={image} alt={title} />
             <strong>{title}</strong>
             <i>{category}</i>
-            {price}
-            <img src={image} alt={title} />
+            price:{price}
+
+            <button onClick={addToCart}>ToCart</button>
+
         </div>
     )
 }
